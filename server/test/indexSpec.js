8943610ecfc85e187ed8f452aa8385b978fd9026
immanojverma2010 <immanojverma2010@gmail.com>
2016-11-15 10:11:17 +0000

    Test passing

--- server/test/indexSpec.js
+++ server/test/indexSpec.js
@@ -3,93 +3,66 @@ var should = require("chai").should(),
 // assert = require("chai").assert,
 
 supertest = require("supertest"),
+server = supertest.agent('http://localhost:8080'),
 app = require("../app");
 
-var url = supertest("http://localhost:8080");
+//var url = supertest("http://localhost:8080");
+
+
 
 describe("Testing the first route", function(err){
-  it("should handle the request", function(done){
-    url
-        .get("/news/")
+  it('login', loginUser());
+  it("should check the reteived favourite news", function(done){
+    server
+        .post("/news/savednews")
         .expect(200)
-        .expect('Content-Type', /json/)
+        .send({"category":"All","searchNews":""
+          })
         .end(function(err,res){
           if (err) {
 
 				        throw err;
 			    }
-          //expect(res.text).to.be.equal("Hello!");
-
-        res.body[0].newid.should.be.equal("720161114T053039Z");
-  //assert(res.text == "Hello!","Test has failed");
-          done();
+        res.body[0].newid.should.be.equal("220161114T074859Z");
+        done();
         });
   });
 });
+
 
 describe("Testing the second route", function(err){
-  it("should handle the post request", function(done){
-    var mov= {"newid": "220161113T182421Z",
-    "author": "Duncan Robinson, Alex Barker, Stefan Wagstyl",
-    "title": "Britain and France snub EU’s emergency Trump meeting",
-    "description": "Split highlights difficulty EU faces co-ordinating response to US presidential election",
-    "url": "http://www.ft.com/cms/s/0/bd74b6de-a9ba-11e6-809d-c9f98a0cf216.html",
-    "urlToImage": "http://im.ft-static.com/content/images/fe977270-da24-4e45-9758-88520d73a6aa.img",
-    "publishedAt": "2016-11-13T18:24:21Z",
-    "category": "economical",
-    "comments": "sdfg"};
-    url
+  it('login', loginUser());
+  it("should check for duplicate news", function(done){
+    server
         .post("/news/add")
-        .send(mov)
         .expect(200)
-
+        .send({"newid":"520161114T050933Z","author":"http://www.abc.net.au/news/julia-holman/4760348",
+        "title":"US 'no dumping ground' for Australia's refugees","description":"The Obama administration could rush through the Australia-United States resettlement deal to get people on Manus Island and Nauru resettled before Donald Trump is sworn in, an immigration expert says.","url":"http://www.abc.net.au/news/2016-11-14/us-expert-questions-refugee-resettlement-plan/8023946",
+        "urlToImage":"http://www.abc.net.au/news/image/6961658-1x1-700x700.jpg","publishedAt":"2016-11-14T05:09:33Z","category":"politics",
+        "comments":"akjkks"
+          })
         .end(function(err,res){
           if (err) {
 
 				        throw err;
 			    }
-          res.text.should.be.equal("News added successfully");
-          //expect(res.text).to.be.equal("Hello!");
-          //assert(res.text == "Hello!","Test has failed");
-          done();
+        res.text.should.be.equal("this News is already present in your favourites");
+        done();
         });
   });
 });
 
-describe("Testing the third route", function(err){
-  it("should handle the delete request", function(done){
-    url
-        .del("/news/remove/220161113T182421Z")
-        .expect(200)
-        .end(function(err,res){
-          if (err) {
+function loginUser() {
+   return function(done) {
+       server
+           .post('/login')
+           .send({ username: 'admin', password: 'admin' })
+           .expect(200)
+           .end(onResponse);
 
-				        throw err;
-			    }
-          res.text.should.be.equal("News deleted");
-          //expect(res.text).to.be.equal("Hello!");
-          //assert(res.text == "Hello!","Test has failed");
-          done();
-        });
-  });
-});
-
-describe("Testing the fourth route", function(err){
-  it("should handle the update request", function(done){
-    var obj={"newid": "120161114T054912Z","comments":"latest updated comments"};
-    url
-        .put("/news/update")
-        .send(obj)
-        .expect(200)
-        .end(function(err,res){
-          if (err) {
-
-				        throw err;
-			    }
-          res.text.should.be.equal("Updated news");
-          //expect(res.text).to.be.equal("Hello!");
-          //assert(res.text == "Hello!","Test has failed");
-          done();
-        });
-  });
-});
+       function onResponse(err, res) {
+          if (err) return done(err);
+          return done();
+       }
+   };
+};
